20bcd32d3ec42915ab90c7a46e72ba91acc7575a
JLChnToZ <jeremylam619@gmail.com>
2022-05-29 18:43:56 +0800

    Pickupable Pool Manager Updates - Use different approach to sync recycling time, which is lesser frequent changes its value. - Add an option to disable auto spawning,   useful for integrate with other gimmicks that needs to control when to spawns. - Auto inactivate all pooled game objects when building world. - Add optional target property to let pooled objects spawn in positions other than its original one,   useful for objects that have a dynamic spawn point. - Minor tweaks on inspector UI.

--- Assets/JLChnToZ/PickupablePoolManager/Scripts/PickupableManager.cs
+++ Assets/JLChnToZ/PickupablePoolManager/Scripts/PickupableManager.cs
@@ -9,12 +9,21 @@ namespace JLChnToZ.VRC {
     [RequireComponent(typeof(VRCObjectSync))]
     [RequireComponent(typeof(VRCPickup))]
     public class PickupableManager: UdonSharpBehaviour {
+        const long customEpoch = 637134336000000000L; // 01/01/2020 00:00 UTC
+        const float ticksPerSecond = System.TimeSpan.TicksPerSecond;
         [SerializeField] PickupablePoolManager poolManager;
+        [Tooltip("Auto respawns if no one touched it for certain time.")]
         [SerializeField] float resetDuration = 300F;
+        [Tooltip("If this object is physics controlled and it is moved too far away to this threshold value before pickuped by someone, it will automatically go back to spawn point. It requires custom spawn target in order to work. Can set to -1 to disable.")]
+        [SerializeField] float respawnDistance = -1;
+        [SerializeField] Transform customSpawnTarget;
         [UdonSynced] bool hasPickuped;
-        [UdonSynced] float resetTimeLeft = float.PositiveInfinity;
+        [UdonSynced] float resetTime = float.PositiveInfinity;
         VRCObjectSync objSync;
+        new Rigidbody rigidbody;
         bool isPickingUp;
+
+        float CurrentTime => (Networking.GetNetworkDateTime().Ticks - customEpoch) / ticksPerSecond;
 
         void Start() {
             if (poolManager == null) {
@@ -22,12 +31,20 @@ namespace JLChnToZ.VRC {
                 if (poolManager == null)
                     Debug.LogError("Object pool is not defined, pickupable manager will not work properly.");
             }
+            rigidbody = GetComponent<Rigidbody>();
+        }
+
+        void OnEnable() {
+            if (!hasPickuped && Networking.IsOwner(gameObject) && customSpawnTarget != null)
+                transform.SetPositionAndRotation(customSpawnTarget.position, customSpawnTarget.rotation);
         }
 
         public override void OnSpawn() {
             if (objSync == null) objSync = (VRCObjectSync)GetComponent(typeof(VRCObjectSync));
             if (Networking.IsOwner(gameObject)) {
                 objSync.Respawn();
+                if (customSpawnTarget != null)
+                    transform.SetPositionAndRotation(customSpawnTarget.position, customSpawnTarget.rotation);
                 hasPickuped = false;
                 ResetTimer();
             }
@@ -37,19 +54,25 @@ namespace JLChnToZ.VRC {
 
         void Update() {
             if (!Networking.IsOwner(gameObject)) return;
-            if (hasPickuped && !isPickingUp && resetTimeLeft > 0) {
-                resetTimeLeft -= Time.deltaTime;
-                if (resetTimeLeft <= 0) _BroadcastReturnToPool();
+            if (hasPickuped) {
+                if (!isPickingUp && resetTime <= CurrentTime)
+                    SendCustomNetworkEvent(NetworkEventTarget.All, nameof(ReturnToPool));
+                return;
             }
+            if (respawnDistance >= 0 &&
+                !rigidbody.isKinematic &&
+                customSpawnTarget != null &&
+                Vector3.Distance(customSpawnTarget.position, transform.position) >= respawnDistance
+            ) OnSpawn();
         }
 
         public override void OnPickup() {
             if (!hasPickuped) {
                 hasPickuped = true;
-                poolManager.SpawnNext();
+                poolManager.AutoSpawnNext();
             }
             isPickingUp = true;
-            resetTimeLeft = float.PositiveInfinity;
+            resetTime = float.PositiveInfinity;
         }
 
         public override void OnDrop() {
@@ -57,17 +80,14 @@ namespace JLChnToZ.VRC {
             ResetTimer();
         }
 
-        void ResetTimer() => resetTimeLeft = resetDuration > 0 ? resetDuration : float.PositiveInfinity;
-
-        public void _BroadcastReturnToPool() {
-            ReturnToPool();
-            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(ReturnToPool));
-        }
+        void ResetTimer() => resetTime = resetDuration > 0 ?
+            CurrentTime + resetDuration :
+            float.PositiveInfinity;
 
         public void ReturnToPool() {
             isPickingUp = false;
             hasPickuped = false;
-            resetTimeLeft = float.PositiveInfinity;
+            resetTime = float.PositiveInfinity;
             gameObject.SetActive(false);
             poolManager._ReturnToPool(gameObject);
         }
--- Assets/JLChnToZ/PickupablePoolManager/Scripts/PickupablePoolManager.cs
+++ Assets/JLChnToZ/PickupablePoolManager/Scripts/PickupablePoolManager.cs
@@ -8,9 +8,15 @@ using VRC.Udon;
 using VRC.Udon.Common;
 using VRC.Udon.Common.Interfaces;
 #if UNITY_EDITOR && !COMPILER_UDONSHARP
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine.SceneManagement;
 using UdonSharpEditor;
+
+using UnityObject = UnityEngine.Object;
 #endif
 
 namespace JLChnToZ.VRC {
@@ -19,18 +25,20 @@ namespace JLChnToZ.VRC {
     public class PickupablePoolManager: UdonSharpBehaviour {
         VRCObjectPool objectPool;
         bool hasShuffled;
+        [SerializeField] bool autoSpawn = true;
         [UdonSynced] bool hasDrain;
 
         void Start() {
             objectPool = (VRCObjectPool)GetComponent(typeof(VRCObjectPool));
-        }
-
-        public override void OnPlayerJoined(VRCPlayerApi player) {
-            if (player.isLocal && Networking.IsOwner(player, gameObject)) DoSpawn();
+            if (Networking.IsOwner(gameObject)) SendCustomEventDelayedFrames(nameof(AutoSpawnNext), 0);
         }
 
         public override void OnOwnershipTransferred(VRCPlayerApi player) {
             if (player.isLocal) Shuffle();
+        }
+
+        public void AutoSpawnNext() {
+            if (autoSpawn) SpawnNext();
         }
 
         public void SpawnNext() {
@@ -57,12 +65,12 @@ namespace JLChnToZ.VRC {
 
         public void _ReturnToPool(GameObject returnObj) {
             if (!Networking.IsOwner(gameObject)) return;
-            if (!Networking.IsOwner(returnObj)) Networking.SetOwner(Networking.GetOwner(gameObject), returnObj);
+            if (!Networking.IsOwner(returnObj)) Networking.SetOwner(Networking.LocalPlayer, returnObj);
             objectPool.Return(returnObj);
             if (!hasDrain) return;
             hasDrain = false;
             RequestSerialization();
-            SendCustomEventDelayedSeconds(nameof(SpawnNext), 1);
+            SendCustomEventDelayedSeconds(nameof(AutoSpawnNext), 1);
         }
     }
 
@@ -70,37 +78,85 @@ namespace JLChnToZ.VRC {
     [CustomEditor(typeof(PickupablePoolManager))]
     internal class PickupablePoolManagerEditor: Editor {
         float batchSetTime = 300;
+        float batchRespawnDistance = -1;
+        Transform batchSpawnTarget;
+        SerializedProperty autoSpawnProperty;
 
         void OnEnable() {
             InitRespawnTime();
+            autoSpawnProperty = serializedObject.FindProperty("autoSpawn");
         }
 
         void InitRespawnTime() {
             var objPool = (target as Component).GetComponent<VRCObjectPool>();
+            bool hasSpawnTime = false;
+            bool hasRespawnDistance = false;
+            bool hasTarget = false;
+            batchSpawnTarget = null;
             foreach (var obj in objPool.Pool)
                 if (obj != null)
-                    foreach (var udon in GetUdonBehaviours<PickupableManager>(obj))
-                        if (udon.publicVariables.TryGetVariableValue("resetDuration", out batchSetTime))
-                            return;
-            batchSetTime = 300;
+                    foreach (var udon in GetUdonBehaviours<PickupableManager>(obj)) {
+                        if (!hasSpawnTime && udon.publicVariables.TryGetVariableValue("resetDuration", out batchSetTime))
+                            hasSpawnTime = true;
+                        if (!hasRespawnDistance && udon.publicVariables.TryGetVariableValue("respawnDistance", out batchRespawnDistance))
+                            hasRespawnDistance = true;
+                        if (udon.publicVariables.TryGetVariableValue("customSpawnTarget", out Transform spawnTarget)) {
+                            if (batchSpawnTarget == null && spawnTarget != null) {
+                                batchSpawnTarget = spawnTarget;
+                                hasTarget = true;
+                            } else if (hasTarget && batchSpawnTarget != spawnTarget) {
+                                batchSpawnTarget = null;
+                            }
+                        }
+                    }
+            if (!hasSpawnTime) batchSetTime = 300;
+            if (!hasRespawnDistance) batchRespawnDistance = -1;
         }
 
         public override void OnInspectorGUI() {
             if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target)) return;
+            EditorGUILayout.PropertyField(autoSpawnProperty);
+            EditorGUILayout.Space();
+
             EditorGUILayout.LabelField("Pickupable Pool Manager Utilities", EditorStyles.boldLabel);
             if (GUILayout.Button("Add all children with handlers to object pool")) DeferStart(AssignPickupableChildren);
             if (GUILayout.Button("Set references to all pooled objects")) DeferStart(AssignPickupable);
             EditorGUILayout.Space();
+
             EditorGUILayout.LabelField("Batch Set Auto Respawn", EditorStyles.boldLabel);
             EditorGUI.BeginChangeCheck();
-            bool autoRespawn = EditorGUILayout.ToggleLeft("Enable Auto Respawn", !float.IsInfinity(batchSetTime));
+            bool autoRespawn = EditorGUILayout.ToggleLeft("Enable (When untouched for certain time)", !float.IsInfinity(batchSetTime));
             bool respawnChanged = EditorGUI.EndChangeCheck();
             if (autoRespawn) {
                 if (respawnChanged) batchSetTime = 0;
-                batchSetTime = EditorGUILayout.FloatField("Respawn Time", batchSetTime);
+                batchSetTime = EditorGUILayout.FloatField("Time", batchSetTime);
             } else if (respawnChanged)
                 batchSetTime = float.PositiveInfinity;
             if (GUILayout.Button("Apply All")) DeferStart(AssignRespawnTime);
+            EditorGUI.BeginChangeCheck();
+            autoRespawn = EditorGUILayout.ToggleLeft("Enable (If before pickuped by anyone but moved by physics)", batchRespawnDistance >= 0);
+            respawnChanged = EditorGUI.EndChangeCheck();
+            if (autoRespawn) {
+                if (respawnChanged) batchRespawnDistance = 0;
+                batchRespawnDistance = EditorGUILayout.FloatField("Distance", batchRespawnDistance);
+            } else if (respawnChanged)
+                batchRespawnDistance = -1;
+            if (GUILayout.Button("Apply All")) DeferStart(AssignResepawnDistance);
+            if (autoRespawn)
+                EditorGUILayout.HelpBox("This feature requires custom spawn position setted, you can batch assign it below.", MessageType.Info);
+            EditorGUILayout.Space();
+
+            EditorGUILayout.LabelField("Batch Set Spawn Position", EditorStyles.boldLabel);
+            batchSpawnTarget = EditorGUILayout.ObjectField("Custom Spawn Position", batchSpawnTarget, typeof(Transform), true) as Transform;
+            EditorGUILayout.BeginHorizontal();
+            if (GUILayout.Button("Assign This")) {
+                batchSpawnTarget = (target as Component).transform;
+                DeferStart(AssignSpawnTarget);
+            }
+            if (GUILayout.Button("Apply All")) DeferStart(AssignSpawnTarget);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.HelpBox("This is for prevents position shifted when parent is moved, or as a reference on measuring distance to its spawn point.", MessageType.Info);
+
             serializedObject.ApplyModifiedProperties();
         }
 
@@ -128,21 +184,27 @@ namespace JLChnToZ.VRC {
             var objPool = targetComponent.GetComponent<VRCObjectPool>();
             if (!(target is UdonBehaviour targetUb))
                 targetUb = UdonSharpEditorUtility.GetBackingUdonBehaviour(target as UdonSharpBehaviour);
-            foreach (var obj in objPool.Pool)
-                if (obj != null)
-                    foreach (var udon in GetUdonBehaviours<PickupableManager>(obj))
-                        if (!TrySetOrAddVariable(udon, "poolManager", targetUb))
-                            Debug.LogWarning($"Failed to assign {targetUb} as poolManager to {udon}.");
+            BatchAssignVariable(objPool.Pool, "poolManager", targetUb);
         }
 
-        void AssignRespawnTime() {
+        void AssignRespawnTime() => BatchAssignVariable("resetDuration", batchSetTime);
+
+        void AssignResepawnDistance() => BatchAssignVariable("respawnDistance", batchRespawnDistance);
+
+        void AssignSpawnTarget() => BatchAssignVariable("customSpawnTarget", batchSpawnTarget);
+
+        void BatchAssignVariable<T>(string variableName, T value) {
             var targetComponent = target as Component;
             var objPool = targetComponent.GetComponent<VRCObjectPool>();
-            foreach (var obj in objPool.Pool)
+            BatchAssignVariable(objPool.Pool, variableName, value);
+        }
+
+        static void BatchAssignVariable<T>(GameObject[] gameObjects, string variableName, T value) {
+            foreach (var obj in gameObjects)
                 if (obj != null)
                     foreach (var udon in GetUdonBehaviours<PickupableManager>(obj))
-                        if (!TrySetOrAddVariable(udon, "resetDuration", batchSetTime))
-                            Debug.LogWarning($"Failed to assign respawn time to {udon}.");
+                        if (!TrySetOrAddVariable(udon, variableName, value))
+                            Debug.LogWarning($"Failed to assign {variableName} to {udon}.");
         }
 
         static IEnumerable<UdonBehaviour> GetUdonBehaviours<T>(Component component) => GetUdonBehaviours<T>(component.gameObject);
@@ -174,9 +236,30 @@ namespace JLChnToZ.VRC {
             return false;
         }
 
-        static void RecordPrefabChangedIfNeeded(Component component) {
-            if (PrefabUtility.IsPartOfPrefabInstance(component))
-                PrefabUtility.RecordPrefabInstancePropertyModifications(component);
+        static void RecordPrefabChangedIfNeeded(UnityObject obj) {
+            if (PrefabUtility.IsPartOfPrefabInstance(obj))
+                PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
+        }
+    }
+
+    internal sealed class PickupablePoolManagerPreprocessor: IProcessSceneWithReport {
+        static readonly Type pickupablePoolManagerType = typeof(PickupablePoolManager);
+        public int callbackOrder => 0;
+
+        public void OnProcessScene(Scene scene, BuildReport report) {
+            foreach (var ub in scene.GetRootGameObjects().SelectMany(go => go.GetComponentsInChildren<UdonBehaviour>(true)))
+                if (
+                    ub != null &&
+                    ub.programSource is UdonSharpProgramAsset usharpAsset &&
+                    usharpAsset.sourceCsScript != null &&
+                    usharpAsset.sourceCsScript.GetClass() == pickupablePoolManagerType
+                ) {
+                    var pool = ub.GetComponent<VRCObjectPool>();
+                    if (pool != null && pool.Pool != null)
+                        foreach (var entry in pool.Pool)
+                            if (entry != null)
+                                entry.SetActive(false);
+                }
         }
     }
 #endif
